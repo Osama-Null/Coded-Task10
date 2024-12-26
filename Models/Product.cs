5cf73bbae8e879da3f1cbd67de3add1e2f37138d
Osama <osamaashkanani@gmail.com>
2024-12-26 11:44:39 +0300

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleT10.Models
+{
+    public class Product
+    {
+        public string proN;
+        public float proP;
+        public int proQ;
+
+        public string GetProduct(string proName)
+        {
+            return $">> Product name: {proName}";
+        }
+        public string GetProduct(string proName, float proPrice)
+        {
+            return $">> Product name: {proName} | Product price: ${proPrice}";
+        }
+        public string GetProduct(string proName, float proPrice, int proQt)
+        {
+            proP = proPrice;
+            proQ = proQt;
+
+            return $">> Product name: {proName} | Product price: ${proPrice} | Qty: x{proQt}";
+        }
+        public void GetTotoal()
+        {
+            Console.WriteLine($"   Total: ${proP * proQ}");
+        }
+
+        //...................................................................................
+        public string Name { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public int Quan { get; set; }
+        public float Sum { get; set; } = 0f;
+        List<Product> proD = new List<Product>();
+
+        public void AddPro(string proName, float proPrice, int proQt)
+        {
+            proD.Add(new Product { Name = proName, Price = proPrice, Quan = proQt });
+        }
+        public void GetProduct()
+        {
+            foreach (var element in proD)
+            {
+                Sum += (element.Price * element.Quan);
+                Console.WriteLine($"{element.Name}\t\t\t${element.Price}\t\tx{element.Quan}\t\t${element.Price * element.Quan}");
+            }
+            Console.WriteLine($"\n>> Total price: ${Sum}");
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleApp10.Models;
+using ConsoleT10.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+Product product = new Product();
+
+string name;
+float price;
+int quan;
+
+Console.Write("Enter product name: ");
+name = Console.ReadLine();
+
+Console.WriteLine(product.GetProduct(name));
+
+//.................................................................
+Console.Write("\nEnter product name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter product price: $");
+price = float.Parse(Console.ReadLine());
+
+Console.WriteLine(product.GetProduct(name, price));
+
+//.................................................................
+Console.Write("\nEnter product name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter product price: $");
+price = float.Parse(Console.ReadLine());
+
+Console.Write("Enter product quantity: x");
+quan = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(product.GetProduct(name, price, quan));
+
+//.................................................................
+product.GetTotoal();
+
+//.................................................................
+int loop;
+
+Console.Write("\n\nNumbers of products you want to add: ");
+loop = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < loop; i++)
+{
+    Console.Write($"\n\nProduct {i + 1} name: ");
+    name = Console.ReadLine();
+
+    Console.Write($"Product {i + 1} price: $");
+    price = float.Parse(Console.ReadLine());
+
+    Console.Write($"Quantity: x");
+    quan = Convert.ToInt32(Console.ReadLine());
+
+    product.AddPro(name, price, quan);
+}
+Console.WriteLine("\n\nName\t\t\tPrice\t\tQuantity\tPrice\n_____________________________________________________________________________");
+product.GetProduct();
